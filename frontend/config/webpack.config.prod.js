4821c3431b6dc9f531eec016f9ebc035462de91a
Michał Malanowicz <malanowicz.michal@gmail.com>
2018-06-23 18:46:18 +0200

    chore: sass

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -205,6 +205,9 @@ module.exports = {
                         ],
                       },
                     },
+                    {
+                        loader: require.resolve('sass-loader'),
+                    },
                   ],
                 },
                 extractTextPluginOptions
@@ -222,7 +225,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/,],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
